8a29114d132ed561d2e5c30842aff2f3cd27ab7a
Andrea Huang <huangandrea@hotmail.it>
2025-04-26 16:18:32 +0200

    add onClose to solve ESC key cases when modal is open (would disalign modalIsOpen state)

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
 
-function Modal({ open, children }) {
+function Modal({ open, children, onClose }) {
   const dialog = useRef();
 
   // This implementation yields an error, because the first time the components are rendered
@@ -28,7 +28,7 @@ function Modal({ open, children }) {
 
   // this implementation does not show the backdrop, because showModal() is not invoked
   return createPortal(
-    <dialog className="modal" ref={dialog}>
+    <dialog className="modal" ref={dialog} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById("modal")
